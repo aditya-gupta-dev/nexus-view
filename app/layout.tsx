ab4808b039b71c4dc98d4559917469c4c963ab23
Aditya Gupta <adityagupta2087@gmail.com>
2026-04-09 19:04:07 +0530

    added shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,9 +28,11 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
+      className={cn("antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body>
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <></>
+    <>
+      <Button>
+        Something
+      </Button>
+    </>
   );
 }
